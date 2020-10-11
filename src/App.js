b0e40b8fe0ec78ba36771c7afd783159e43d7c8d
mustafa1133 <44012227+mustafa1133@users.noreply.github.com>
2020-10-10 22:05:08 -0400

    Added sign in Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+
 import './pages/homepage/homepage.styles.scss'
 import {Switch,Route} from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component.jsx'
-import Header from './Components/header/header-component.jsx'
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
+import Header from './Components/header/header-component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 function App() {
   return (
     <div>
@@ -12,6 +15,7 @@ function App() {
       <Switch>
     <Route exact path='/' component={HomePage}></Route>
     <Route path='/shop' component={ShopPage}></Route>
+    <Route path='/signin' component={SignInAndSignUpPage}></Route>
 
       </Switch>
     </div>
