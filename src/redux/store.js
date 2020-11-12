4f47d5120d2ff27a8ced58ce39183932296e8e46
mustafa1133 <mustafa1133@hotmail.com>
2020-11-12 08:51:47 -0500

    added comment

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,10 @@ import logger from 'redux-logger';
 import {persistStore} from 'redux-persist';
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [logger]; 
 
 if(process.env.NODE_ENV === 'development'){
-   middlewares.push(logger); 
+   middlewares.push(logger);   // make sure we dont log in live 
 }
 
 export const store = createStore(rootReducer,applyMiddleware(...middlewares));
